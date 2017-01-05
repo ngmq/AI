0bd247758c099490a1d7aa89754d04153e8beaec
ngmq <manhquan233@gmail.com>
2017-01-05 13:54:48 +0100

    Done

--- TestKeras/myKeraCode.py
+++ TestKeras/myKeraCode.py
@@ -21,7 +21,17 @@ print type(dataset) # numpy.ndarray
 model = Sequential()
 model.add(Dense(12, input_dim = 8, init = 'uniform', activation = 'relu'))
 model.add(Dense(8, init = 'uniform', activation = 'relu'))
-model.add(Dens(1, init = 'uniform', activation = 'sigmoid'))
+model.add(Dense(1, init = 'uniform', activation = 'sigmoid'))
 
 # Compile model
 model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+# Fit model 
+model.fit(X, Y, nb_epoch = 150, batch_size = 10)
+
+# Evaluate model 
+scores = model.evaluate(X, Y)
+
+print model.metrics_names
+print scores
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100) )
